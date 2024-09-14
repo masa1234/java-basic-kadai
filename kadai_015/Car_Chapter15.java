bea515e142005125cfd48e1e7ba3156b443f0acf
Masa <17587674+masa1234@users.noreply.github.com>
2024-09-14 13:43:32 +0900

    再修正

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -10,15 +10,16 @@ public class Car_Chapter15 {
       System.out.println("ギアを" + this.gear + "から" + afterGear + "に切り替えました");
       afterGear = 1;
       gear = afterGear;
+      this.speed = speed * gear;
     } else {
       System.out.println("ギアを" + this.gear + "から" + afterGear + "に切り替えました");
       gear = afterGear;
-
+      this.speed = speed * gear;
     }
   }
 
   public void run() {
-    System.out.println("速度は時速" + (gear * speed) + "kmです");
+    System.out.println("速度は時速" + this.speed + "kmです");
   }
 
 }
