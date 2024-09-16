c8014f9b602289898ebb365d0de0a4afdea0e26d
masa1234 <17587674+masa1234@users.noreply.github.com>
2024-09-16 10:11:58 +0900

    課題18

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,24 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18 {
+  // 姓を表す
+  public String familyName;
+  // 名を表す
+  public String givenName;
+  // 住所を表す
+  public String address;
+
+  // 共通の紹介を出力する
+  public void commonIntroduce() {
+
+  }
+
+  // 個別の紹介を出力する
+  public abstract void eachIntroduce();
+
+  // 紹介を実行する
+  public void execIntroduce() {
+
+  }
+
+}
